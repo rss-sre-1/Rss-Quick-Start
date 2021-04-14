7a1f19ef5df5c73e542a45e354506f189fb4081e
J Schmitz <51449182+joneschmitz@users.noreply.github.com>
2021-04-14 10:21:53 -0500

    Update superkube.sh

--- Scripts/superkube.sh
+++ Scripts/superkube.sh
@@ -2,22 +2,22 @@
 
 if [[ $1 == "get" && $2 != "all" ]]; then
 echo "=========Kubernetes========"
-kubectl -n kube-node-lease $@
-kubectl -n kube-public $@
-kubectl -n kube-system $@
+  kubectl -n kube-node-lease $@
+  kubectl -n kube-public $@
+  kubectl -n kube-system $@
 echo "==========Default=========="
-kubectl -n default $@
+  kubectl -n default $@
 echo "=========Docutest=========="
-kubectl -n docutest $@
+  kubectl -n docutest $@
 echo "=========Postgres=========="
-kubectl -n postgres $@
+  kubectl -n postgres $@
 echo "============RSS============"
-kubectl -n rss-account $@
-kubectl -n rss-cart $@
-kubectl -n rss-evaluation $@
-kubectl -n rss-frontend $@
-kubectl -n rss-inventory $@
+  kubectl -n rss-account $@
+  kubectl -n rss-cart $@
+  kubectl -n rss-evaluation $@
+  kubectl -n rss-frontend $@
+  kubectl -n rss-inventory $@
 else
-kubectl $@
+  kubectl $@
 fi
 
